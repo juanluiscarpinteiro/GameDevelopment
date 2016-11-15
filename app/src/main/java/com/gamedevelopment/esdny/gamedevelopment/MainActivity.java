29cc80067fcf2aff78dce01cdcefa154c9122716
Juan Carpinteiro <carptyk04@gmail.com>
2016-11-14 21:05:12 -0500

    delay on swap done with checking included

--- app/src/main/java/com/gamedevelopment/esdny/gamedevelopment/MainActivity.java
+++ app/src/main/java/com/gamedevelopment/esdny/gamedevelopment/MainActivity.java
@@ -2,6 +2,7 @@ package com.gamedevelopment.esdny.gamedevelopment;
 
 import android.app.Activity;
 import android.content.Context;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -19,6 +20,7 @@ public class MainActivity extends Activity {
     int count=1;
     int lastTouch;
     final ImageAdapter adapter = new ImageAdapter(this);
+    Handler handler = new Handler();
 
     public Integer[] aNumbers = {R.drawable.one,R.drawable.two,
             R.drawable.three,R.drawable.four,
@@ -52,11 +54,13 @@ public class MainActivity extends Activity {
             R.drawable.emoji_sexface,R.drawable.emoji_sexface,
             R.drawable.angel_emoji,R.drawable.angel_emoji,
             R.drawable.shy_emoji};
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        GridView gr = (GridView) findViewById(R.id.gridView1);
+        final GridView gr = (GridView) findViewById(R.id.gridView1);
 
         gr.setAdapter (adapter);
         gr.setOnItemClickListener(new AdapterView.OnItemClickListener(){
@@ -64,10 +68,12 @@ public class MainActivity extends Activity {
                                     View v, int position, long id)
             {
                 Toast.makeText(getBaseContext(),
-                        "pic" + (position + 1) + " selected",
+                        "pic" + (position + 1) + " selected " +"count "+ count ,
                         Toast.LENGTH_SHORT).show();
 
-                checkForMatch(position);
+                checkForMatch(position,gr);
+               // gr.setEnabled(true);
+
                 adapter.notifyDataSetChanged();
                 count++;
 
@@ -119,42 +125,77 @@ public class MainActivity extends Activity {
 
 
 
-    public void checkForMatch(int position1)
+    public void checkForMatch(int position1,GridView gridView)
     {
-        aNumbers[position1]=emojis[position1];
 
 
         if (count % 2==1)
         {
+            aNumbers[position1]=emojis[position1];
+            adapter.notifyDataSetChanged();
             lastTouch=position1;
 
         }
 
         else if (count %2== 0)
         {
-            if( alphabet1[lastTouch].equalsIgnoreCase(alphabet1[position1]))
-            {
-                Toast.makeText(getApplicationContext(),
-                        " match maid "+ "this is last touch "+lastTouch + "this is current " + position1 , Toast.LENGTH_SHORT).show();
+            gridView.setEnabled(false);
+            aNumbers[position1]=emojis[position1];
+            adapter.notifyDataSetChanged();
 
 
-                //adapter.notifyDataSetChanged();
-                count=0;
-            }
-            else {
-
-                aNumbers[lastTouch]=OriginalLoadingScreen[lastTouch];
-                //loadscreen2[position]=" # ";
-                aNumbers[position1]=OriginalLoadingScreen[position1];
-                //adapter.notifyDataSetChanged();
+            if( alphabet1[lastTouch].equalsIgnoreCase(alphabet1[position1])&& lastTouch!= position1)
+                {
+                    Toast.makeText(getApplicationContext(),
+                            " match maid "+ "this is last touch "+lastTouch + "current is " + position1 + " count "+ count , Toast.LENGTH_SHORT).show();
 
 
-            }
+                    adapter.notifyDataSetChanged();
+                    count=0;
+                    gridView.setEnabled(true);
+                    gridView.getChildAt(lastTouch).setClickable(false);
+                    gridView.getChildAt(position1).setEnabled(false);
+
+                }
+                else {
+
+                 Toast.makeText(getApplicationContext(),
+                         " match not maid "+ "this is last touch "+lastTouch + "current is " + position1 + " count "+ count , Toast.LENGTH_SHORT).show();
+
+
+                    setOriginalImages(position1, gridView);
+
+
+                    count =0;
+
+
+                }
+
 
 
 
         }
     }
+    public void setOriginalImages(int position,GridView gridView)
+    {
+        final GridView gr =gridView;
+        final int positionLocal= position;
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                aNumbers[lastTouch]=OriginalLoadingScreen[lastTouch];
+                //loadscreen2[position]=" # ";
+                aNumbers[positionLocal]=OriginalLoadingScreen[positionLocal];
+                adapter.notifyDataSetChanged();
+                gr.setEnabled(true);
+            }
+        }, 2000);
+
+    }
+
+
 
 
 
